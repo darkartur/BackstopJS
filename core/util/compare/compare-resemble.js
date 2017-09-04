aa695e30be0ca637bce00534549e947f9f545d85
Arthur Udalov <AEUdalov.SBT@sberbank.ru>
2017-09-04 20:41:08 +0300

    Compare rawMismatch

--- core/util/compare/compare-resemble.js
+++ core/util/compare/compare-resemble.js
@@ -4,7 +4,7 @@ module.exports = function (referencePath, testPath, misMatchThreshold, resembleO
   return new Promise(function (resolve, reject) {
     resemble.outputSettings(resembleOutputSettings || {});
     resemble(referencePath).compareTo(testPath).onComplete(data => {
-      if ((requireSameDimensions === false || data.isSameDimensions === true) && data.misMatchPercentage <= misMatchThreshold) {
+      if ((requireSameDimensions === false || data.isSameDimensions === true) && data.rawMisMatchPercentage <= misMatchThreshold) {
         return resolve(data);
       }
       reject(data)
